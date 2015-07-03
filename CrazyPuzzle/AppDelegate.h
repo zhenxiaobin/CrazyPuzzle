2729277ccddfe09a2e146117a0fbae4f96028b8b
SevenMay <xiaobin_zhen@hotmail.com>
2015-07-03 15:53:06 +0800

    Initial Commit

---
+++ CrazyPuzzle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrazyPuzzle
+//
+//  Created by tsmc on 15/7/3.
+//  Copyright (c) 2015年 SevenMay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrazyPuzzle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CrazyPuzzle
+//
+//  Created by tsmc on 15/7/3.
+//  Copyright (c) 2015年 SevenMay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
